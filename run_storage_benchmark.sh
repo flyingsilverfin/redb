24958e45b0f0bad4b7be41c86055d47d9bbc35f8
Ganeshwara Hananda <ganesh@vaticle.com>
2024-10-01 15:42:29 +0100

    Refactor

---
+++ run_storage_benchmark.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cargo bench --bench storage_benchmark -- s 1 /tmp/ > s-1.md
+cargo bench --bench storage_benchmark -- s 4 /tmp/ > s-4.md
+cargo bench --bench storage_benchmark -- s 16 /tmp/ > s-16.md
+
+cargo bench --bench storage_benchmark -- m 1 /tmp/ > m-1.md
+cargo bench --bench storage_benchmark -- m 4 /tmp/ > m-4.md
+cargo bench --bench storage_benchmark -- m 16 /tmp/ > m-16.md
