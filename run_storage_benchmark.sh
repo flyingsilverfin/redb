b99ebcb71cfd07b1819e4b3ac5ae0b67ee84ca3a
Ganeshwara Hananda <ganesh@vaticle.com>
2024-10-01 15:59:25 +0100

    Refactor

--- run_storage_benchmark.sh
+++ run_storage_benchmark.sh
@@ -2,10 +2,13 @@
 
 set -ux
 
-cargo bench --bench storage_benchmark -- s 1 $TMP_DIR > s-1.md
-cargo bench --bench storage_benchmark -- s 4 $TMP_DIR > s-4.md
-cargo bench --bench storage_benchmark -- s 16 $TMP_DIR > s-16.md
+TMP_DIR=$1
+OP_SIZE=('s' 'm' 'b')
+THREAD_COUNT=(1 4 16)
 
-cargo bench --bench storage_benchmark -- m 1 $TMP_DIR > m-1.md
-cargo bench --bench storage_benchmark -- m 4 $TMP_DIR > m-4.md
-cargo bench --bench storage_benchmark -- m 16 $TMP_DIR > m-16.md
+for op_size in "${OP_SIZE[@]}"; do
+  for thread_count in "${THREAD_COUNT[@]}"; do
+    echo "$op_size, $thread_count"
+    cargo bench --bench storage_benchmark -- "$op_size" "$thread_count" "$TMP_DIR" > "$op_size-$thread_count.md"
+  done
+done
