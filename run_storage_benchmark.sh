70fc0b3cf13252a0b461b5d884f9490935184181
Ganeshwara Hananda <ganesh@vaticle.com>
2024-10-01 16:51:35 +0100

    Refactor

--- run_storage_benchmark.sh
+++ run_storage_benchmark.sh
@@ -2,15 +2,13 @@
 
 set -ux
 
-echo "Benchmark version: $(git rev-parse HEAD)"
-
 TMP_DIR=$1
 OP_SIZE=('s' 'm' 'b')
 THREAD_COUNT=(1 4 16)
 
 for op_size in "${OP_SIZE[@]}"; do
   for thread_count in "${THREAD_COUNT[@]}"; do
-    echo "$op_size, $thread_count"
-    cargo bench --bench storage_benchmark -- "$op_size" "$thread_count" "$TMP_DIR" > "$op_size-$thread_count.md"
+    echo "Benchmark version: $(git rev-parse HEAD)" > "$op_size-$thread_count.md"
+    cargo bench --bench storage_benchmark -- "$op_size" "$thread_count" "$TMP_DIR" >> "$op_size-$thread_count.md"
   done
 done
