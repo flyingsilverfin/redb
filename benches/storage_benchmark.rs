a60e2f69c324eb75efc8571b80ba321ab06161fb
Ganeshwara Hananda <ganesh@vaticle.com>
2024-10-02 15:57:47 +0100

    Revert "Optimise rocksdb"
    This reverts commit bca402c57297575897ccf4c447739ad5113d276e.

--- benches/storage_benchmark.rs
+++ benches/storage_benchmark.rs
@@ -30,25 +30,8 @@ fn main() {
 
 fn rocksdb_benchmark(op_size: &OpSize, thread_count: usize, tmpdir_path: &String) {
     let tmpdir = TempDir::new_in(tmpdir_path).unwrap();
-
-    let mut bb = rocksdb::BlockBasedOptions::default();
-    bb.set_block_cache(&rocksdb::Cache::new_lru_cache(4 * 1_024 * 1_024 * 1_024)); // TODO
-    // setFilterPolicy non-existent
-    bb.set_partition_filters(true);
-    bb.set_index_type(rocksdb::BlockBasedIndexType::TwoLevelIndexSearch);
-    bb.set_optimize_filters_for_memory(true);
-    bb.set_pin_top_level_index_and_filter(true);
-    bb.set_pin_l0_filter_and_index_blocks_in_cache(true);
-    bb.set_cache_index_and_filter_blocks(true);
-    // setCacheIndexAndFilterBlocksWithHighPriority non-existent
-
     let mut rocksdb_opts = rocksdb::Options::default();
     rocksdb_opts.create_if_missing(true);
-    rocksdb_opts.set_max_subcompactions(thread_count.to_u32().unwrap());
-    rocksdb_opts.set_max_background_jobs(thread_count.to_i32().unwrap());
-    rocksdb_opts.set_enable_write_thread_adaptive_yield(true);
-    rocksdb_opts.set_allow_concurrent_memtable_write(true);
-    rocksdb_opts.set_block_based_table_factory(&bb);
     let rocksdb_db = rocksdb::OptimisticTransactionDB::open(&rocksdb_opts, tmpdir.path()).unwrap();
     let rocksdb_driver = OptimisticRocksdbBenchDatabase::new(&rocksdb_db);
     preload(&rocksdb_driver, &op_size, thread_count);
