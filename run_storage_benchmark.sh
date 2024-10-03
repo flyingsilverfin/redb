c63ada548d10fe210d63dfe243a7da9ed842ee3a
Ganeshwara Hananda <ganesh@vaticle.com>
2024-10-03 16:56:47 +0100

    refactor run_storage_benchmark.sh

--- run_storage_benchmark.sh
+++ run_storage_benchmark.sh
@@ -2,22 +2,23 @@
 
 set -ux
 
-git_version=$(git rev-parse HEAD)
-DIR="$1"
-THREAD_COUNT=(1)
+RUN_BASE_DIR="$1"
+OP_SIZES="${@:2}"
 
-for thread_count in "${THREAD_COUNT[@]}"; do
-  child_dir_name="${@:2}-$thread_count"
-  child_dir="$DIR/$child_dir_name"
-  rm -rf "$child_dir"
-  mkdir -p "$child_dir"
-  child_log_file="$child_dir/log.md"
+THREAD_COUNTS=(1)
+
+for thread_count in "${THREAD_COUNTS[@]}"; do
+  run_dir_name="${OP_SIZES// /-}-$thread_count"
+  run_dir="$RUN_BASE_DIR/$run_dir_name"
+  rm -rf "$run_dir"
+  mkdir -p "$run_dir"
+  child_log_file="$run_dir/log.md"
   echo "Benchmark version: $(git rev-parse HEAD)" >> "$child_log_file"
   echo >> "$child_log_file"
   for op_size in "${@:2}"; do
     echo "Operation: {op_size}"
     date >> "$child_log_file"
-    RUST_BACKTRACE=full cargo bench --bench storage_benchmark -- "$op_size" "$thread_count" "$child_dir" >> "$child_log_file"
+    RUST_BACKTRACE=full cargo bench --bench storage_benchmark -- "$op_size" "$thread_count" "$run_dir" >> "$child_log_file"
     echo >> "$child_log_file"
   done
 done
