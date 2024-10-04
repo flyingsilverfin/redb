c5d1c698d71baadfda368abd9ac541c31a35f80a
Ganeshwara Hananda <ganesh@vaticle.com>
2024-10-04 15:58:14 +0100

    created mi3 op size

--- benches/storage_op_size.rs
+++ benches/storage_op_size.rs
@@ -13,10 +13,11 @@ impl OpSize {
             "s" => SMALL,
             "m" => MEDIUM,
             "mi2" => MEDIUM_I2,
+            "mi3" => MEDIUM_I3,
             "m2" => MEDIUM2,
             "m3" => MEDIUM3,
             "b" => BIG,
-            x => panic!("must be either 's', 'm', 'mi2', 'm2', 'm3', or 'b', got {}", x)
+            x => panic!("must be either 's', 'm', 'mi2', 'mi3', 'm2', 'm3', or 'b', got {}", x)
         }
     }
 }
@@ -47,6 +48,15 @@ const MEDIUM_I2: OpSize = OpSize {
     scan_per_tx_count: 1_000,
     iter_per_scan_count: 10,
 };
+
+const MEDIUM_I3: OpSize = OpSize {
+    insert_key_total_count: 10_000_000,
+    insert_key_per_tx_count: 100,
+    scan_total_count: 1_00_000,
+    scan_per_tx_count: 1_000,
+    iter_per_scan_count: 10,
+};
+
 
 const MEDIUM2: OpSize = OpSize {
     insert_key_total_count: 100_000_000,
