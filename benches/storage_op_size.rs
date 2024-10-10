9546522250f85f73a813869cf9f320761857402c
joshua <joshua@vaticle.com>
2024-10-10 11:09:53 +0100

    Change Big to do fewer, long scans

--- benches/storage_op_size.rs
+++ benches/storage_op_size.rs
@@ -75,10 +75,10 @@ const MEDIUM3: OpSize = OpSize {
 };
 
 const BIG: OpSize = OpSize {
-    insert_key_total_count: 1000_000_000,
+    insert_key_total_count: 1_000_000_000,
     insert_key_per_tx_count: 1_000,
-    scan_total_count: 10_000_000,
-    scan_per_tx_count: 100,
-    iter_per_scan_count: 10,
+    scan_total_count: 100_000,
+    scan_per_tx_count: 10,
+    iter_per_scan_count: 10_000_000,
 };
 
